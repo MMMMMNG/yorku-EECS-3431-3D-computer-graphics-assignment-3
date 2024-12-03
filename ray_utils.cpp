1a7dcc5823fb0fffe7b02f178dc4a239028de489
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2024-12-03 17:17:36 -0500

    try fix all white issue

--- ray_utils.cpp
+++ ray_utils.cpp
@@ -104,31 +104,31 @@ void rayTraceAllPixels(const Scene &scene, unsigned char* pixels) {
 
     // Step sizes for each pixel on the near plane
     float dx = (scene.r - scene.l) / scene.x;
-    float dy = (scene.b - scene.t) / scene.y;
+    float dy = (scene.t - scene.b) / scene.y;
 
     for (int j = 0; j < scene.y; ++j) {
         for (int i = 0; i < scene.x; ++i) {
             // Compute the pixel position on the near plane
             float px = scene.l + (i + 0.5f) * dx;  // Center of the pixel in X
-            float py = scene.b + (j + 0.5f) * dy;  // Center of the pixel in Y
+            float py = scene.t - (j + 0.5f) * dy;  // Center of the pixel in Y (flip Y-axis)
             float pz = -scene.n;                   // Near plane is at -n in camera space
 
             // Define the ray
             glm::vec4 pixelPos(px, py, pz, 1.0f);
-            glm::vec4 direction = pixelPos - origin;
-            Ray ray = Ray{origin, direction};
+            glm::vec4 direction = glm::normalize(pixelPos - origin); // Normalize direction
+            Ray ray = Ray{origin, glm::vec4(direction.x, direction.y, direction.z, 0.0f)};
 
-            int pixOffset = 3*(i + j * scene.x);
-            if(findAnyHitWithAllObjects(ray, scene)){
-                pixels[pixOffset] =  0;
-                pixels[pixOffset+1] =  0;
-                pixels[pixOffset+2] =  0;
-            }else{
-                pixels[pixOffset] =  255;
-                pixels[pixOffset+1] =  255;
-                pixels[pixOffset+2] =  255;
+            // Determine pixel color
+            int pixOffset = 3 * (i + j * scene.x);
+            if (findAnyHitWithAllObjects(ray, scene)) {
+                pixels[pixOffset] = 0;
+                pixels[pixOffset + 1] = 0;
+                pixels[pixOffset + 2] = 0;
+            } else {
+                pixels[pixOffset] = 255;
+                pixels[pixOffset + 1] = 255;
+                pixels[pixOffset + 2] = 255;
             }
-            
         }
     }
 }
