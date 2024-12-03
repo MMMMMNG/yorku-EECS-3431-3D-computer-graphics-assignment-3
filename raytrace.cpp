2a641e0aa6e6271c400b4863ef08f194260e11c2
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2024-12-03 10:47:56 -0500

    include glm deps

--- raytrace.cpp
+++ raytrace.cpp
@@ -2,6 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp> // glm::pi
 
 using namespace std;
 
@@ -101,6 +107,8 @@ struct Light {
     string outputFile; // no spaces or special characters
 
 int main(int argc, char *argv[]) {
+
+    
 
     // make sure file is provided
     if (argc != 2) {
